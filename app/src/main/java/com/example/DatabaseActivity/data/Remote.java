ecc8c946bf93f6f46901571248f5a030598d3241
Daniel Paiva <daniel.paiva@exaud.com>
2019-06-13 12:59:42 +0100

    Gson applied to Remote and old remote removed

--- app/src/main/java/com/example/DatabaseActivity/data/Remote.java
+++ app/src/main/java/com/example/DatabaseActivity/data/Remote.java
@@ -10,12 +10,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.DatabaseActivity.MyApplication;
 import com.google.gson.Gson;
-import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -30,6 +25,7 @@ class Remote {
     private String TAG = "REMOTE EXECUTION";
 
     private Set<SendData> sendDataSet = new HashSet<>();
+
     private Remote() {
     }
 
@@ -46,7 +42,6 @@ class Remote {
 
                 RequestQueue queue = Volley.newRequestQueue(MyApplication.getContext());
                 final String url = "http://dummy.restapiexample.com/api/v1/employees";
-                //final ArrayList<Employee> employeesList = new ArrayList<>();
 
                 StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                         //Listener
@@ -54,54 +49,14 @@ class Remote {
                             @Override
                             public void onResponse(String response) {
                                 Gson gson = new Gson();
-                                Type dataListType = new TypeToken<List<Employee>>() {}.getType();
-                                ArrayList<Employee> lss = gson.fromJson(response, dataListType);
+                                Type dataListType = new TypeToken<List<Employee>>() {
+                                }.getType();
+                                ArrayList<Employee> employeesList = gson.fromJson(response, dataListType);
 
-
-//                                JSONArray jsonArray = null;
-//
-//                                // Create JSON array with the string given at the URL
-//                                try {
-//                                    jsonArray = new JSONArray(response);
-//
-//                                } catch (JSONException e) {
-//                                    e.printStackTrace();
-//                                }
-//
-//                                // Return ("terminate") Listener if jsonArray is null or empty
-//                                if (jsonArray == null || jsonArray.length() == 0){
-//                                    return;
-//                                }
-//
-//                                // Read each JSONObject in the JSONArray
-//                                for (int i = 0; i < jsonArray.length(); i++) {
-//                                    try {
-//                                        // Each JSONObject represents an Employee
-//                                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-//
-//                                        String objectContent = String.valueOf(jsonObject);
-//                                        Employee employeeToAdd = gson.fromJson(objectContent, Employee.class);
-//
-//                                        /*Long id = jsonObject.getLong("id");
-//                                        String employeeName = jsonObject.getString("employeeName");
-//                                        int employeeSalary = jsonObject.getInt("employeeSalary");
-//                                        int employeeAge = jsonObject.getInt("employeeAge");
-//                                        String profileImage = jsonObject.getString("profileImage");
-//
-//                                        // Create an Object Employee with the data in the JSONObject
-//                                        Employee employeeToAdd = new Employee(id, employeeName,
-//                                                employeeSalary, employeeAge, profileImage);*/
-//
-//                                        // Add the Employee to an ArrayList
-//                                        employeesList.add(employeeToAdd);
-//                                    } catch (JSONException e) {
-//                                        e.printStackTrace();
-//                                    }
-//                                }
 
                                 // SendData from Remote to Activity
                                 for (SendData callback : sendDataSet) {
-                                    callback.sendData(lss);
+                                    callback.sendData(employeesList);
                                 }
 
                             }
@@ -129,8 +84,7 @@ class Remote {
     }
 
 
-
-    public interface SendData{
+    public interface SendData {
         void sendData(ArrayList<Employee> employeeList);
     }
 
